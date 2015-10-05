98a2b806780690befb4db70d28c41967d9afbd65
tacone <tacone@users.noreply.github.com>
2015-10-05 22:05:12 +0200

    add tests for infinite trait

--- src/Filter/InfiniteTrait.php
+++ src/Filter/InfiniteTrait.php
@@ -9,6 +9,9 @@ trait InfiniteTrait
      * Turns the pipe into an infinite stream by auto rewinding once it reachs
      * the end.
      *
+     * WARNING: toArray() will lead to an infinite loop if there is no limit()
+     * or other flow interrupting operations *after* infinite()
+     *
      * @return mixed
      */
 
---
+++ tests/InfiniteTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Pipes\Test;
+
+class InfiniteTest extends BaseTestCase
+{
+
+    public function testInfinite()
+    {
+        $array = array_slice($this->associative(), 0,2);
+        $obj = p($array)->infinite();
+
+        $result = [];
+        $i = 0;
+        foreach ($obj as $value) {
+            $result[] = $value;
+            $i++;
+            if ($i >= 6) break;
+        }
+
+        $expected = [
+             'apples',  'bananas',
+             'apples',  'bananas',
+             'apples',  'bananas',
+        ];
+
+        $this->assertSame($expected, $result);
+    }
+
+
+}
