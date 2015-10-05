64035b0a1312872940b4d97d5bb3c6458678ffd0
tacone <tacone@users.noreply.github.com>
2015-10-05 22:13:31 +0200

    easen the sleep test a little bit

--- tests/SleepTest.php
+++ tests/SleepTest.php
@@ -7,13 +7,13 @@ class SleepTest extends BaseTestCase
 
     public function testSleep()
     {
-        foreach ([0.005, 0.01, 0.015] as $delay) {
+        foreach ([0.01, 0.02, 0.03] as $delay) {
             $array = array_slice($this->associative(), 0, 1);
             $obj = p($array)->sleep($delay);
             $start = microtime(true);
             $array2 = $obj->toArray();
             $end = microtime(true);
-            $this->assertEquals($delay, $end - $start, "", 0.001);
+            $this->assertEquals($delay, $end - $start, "", 0.05);
             $this->assertEquals($array, $array2);
         }
     }
